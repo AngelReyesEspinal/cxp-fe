778f6071a91692eb35ccc2210e60bb312cdaaae5
Angel Arsenio Reyes Espinal <angel.reyes@solvex.com.do>
2020-11-12 21:05:03 -0400

    commit

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000';
+const baseURL = process.env.REACT_APP_API_URL;
 
 const get = async (endpoint) => {
     return axios.get(`${baseURL}/${endpoint}`);
